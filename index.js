610aa7dbdb0763c7ea84f0423dfe1d402d67de7b
Fatolu Stephen <smjssp@gmail.com>
2023-11-02 17:12:37 +0100

    Validate post request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -23,8 +24,13 @@ app.get('/api/courses/:id', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send('Name should not be less than 3 characters ')
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+
+  const result = Joi.validate(req.body, schema)
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message)
     return
   }
   const course = {
