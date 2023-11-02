53d8e9a6fdc42d4473dfb5d044d7f90b88639026
Fatolu Stephen <smjssp@gmail.com>
2023-11-02 17:40:48 +0100

    Handle put request

--- index.js
+++ index.js
@@ -24,15 +24,12 @@ app.get('/api/courses/:id', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-
-  const result = Joi.validate(req.body, schema)
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message)
+  const { error } = validateCourse(req.body)
+  if (error) {
+    res.status(400).send(error.details[0].message)
     return
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -41,6 +38,28 @@ app.post('/api/courses', (req, res) => {
   courses.push(course)
   res.send(course)
 })
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) res.status(404).send('The requested resource was not found')
+
+  const { error } = validateCourse(req.body)
+  if (error) {
+    res.status(400).send(error.details[0].message)
+    return
+  }
+
+  course.name = req.body.name
+  res.send(course)
+})
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+
+  return (result = Joi.validate(course, schema))
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on ${port}`))
